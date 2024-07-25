0e17327a11bdaa05cad408a9f836768f3d4bdfbb
Neel Raval <nraval@ankrhealth.com>
2024-07-25 14:43:50 +0530

    restricting iframe

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 $(document).ready(function () {
+  const allowedOrigins = [
+    "http://localhost:5500",
+    "http://localhost:8080",
+    "https://auth.800ambi.com",
+  ];
+
+  let validParent = false;
+  if (window.top !== window.self) {
+    const parentUrl = new URL(document.referrer);
+    if (allowedOrigins.includes(parentUrl.origin)) {
+      validParent = true;
+    }
+  }
+
+  if (!validParent) {
+    $(".glass-card").html("<h1>Access Denied</h1>");
+    return;
+  }
+
   const testimonials = [
     {
       name: "John Doe",
       title: "Software Engineer",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel orci eget quam aliquet cursus ut sed sapien.",
-      image: "assets/arm.jpeg",
+      image: "https://neel-raval.github.io/leftIframe/assets/arm.jpeg",
     },
     {
       name: "Jane Smith",
       title: "Product Manager",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel orci eget quam aliquet cursus ut sed sapien.",
-      image: "assets/av.jpeg",
+      image: "https://neel-raval.github.io/leftIframe/assets/av.jpeg",
     },
     {
       name: "Michael Brown",
       title: "UX Designer",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel orci eget quam aliquet cursus ut sed sapien.",
-      image: "assets/nk.jpeg",
+      image: "https://neel-raval.github.io/leftIframe/assets/nk.jpeg",
     },
   ];
 
